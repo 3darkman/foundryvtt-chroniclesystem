062aa8a40a2ba91bfb5d4ebbb43bf9817a7846de
Kirlian Silvestre <kirlian.silvestre@gmail.com>
2022-03-06 09:31:58 -0300

    limiting what types of items can be added to a character

--- module/actors/sheets/csCharacterActorSheet.js
+++ module/actors/sheets/csCharacterActorSheet.js
@@ -10,6 +10,14 @@ import SystemUtils from "../../utils/systemUtils.js";
 
 
 export class CSCharacterActorSheet extends CSActorSheet {
+  itemTypesPermitted = [
+      "ability",
+      "weapon",
+      "armor",
+      "equipment",
+      "benefit",
+      "drawback"
+  ]
 
   /** @override */
   static get defaultOptions() {
@@ -372,6 +380,7 @@ export class CSCharacterActorSheet extends CSActorSheet {
     let data;
     try {
       data = JSON.parse(event.dataTransfer.getData('text/plain'));
+
     }
     catch (err) {
       return;
@@ -391,7 +400,8 @@ export class CSCharacterActorSheet extends CSActorSheet {
       if (item) {
         embeddedItem.push(this.actor.getEmbeddedDocument("Item", item.data._id));
       } else {
-        itemsToCreate.push(doc);
+        if (this.itemTypesPermitted.includes(doc.type))
+          itemsToCreate.push(doc);
       }
     });
 
